0a965297481d663853eb79776c8f04278d431b6e
Rich Sharp <richpsharp@gmail.com>
2020-04-03 11:40:01 -0700

    tracking a dockerfile for the geoserver

--- scripts/geoserver_node_startup.sh
+++ scripts/geoserver_node_startup.sh
@@ -1,14 +1,14 @@
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
