a49a84c5ea3ac3d0234abe22e38953b0c3ecb594
Rich Sharp <richpsharp@gmail.com>
2020-04-04 21:38:09 -0700

    smaller memory

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=/mnt/data_dir
-export JAVA_OPTS=-Xmx4g
+export JAVA_OPTS=-Xmx2g
 cd $GEOSERVER_HOME
 exec java $JAVA_OPTS -DGEOSERVER_DATA_DIR="$GEOSERVER_DATA_DIR" -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar
