eb95eeee9b04d5bd6c45237538ceecc10fb91401
Rich Sharp <richpsharp@gmail.com>
2020-04-06 12:28:26 -0700

    trying parens and also emacs

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=/usr/local/geoserver/data_dir
-export JAVA_OPTS="-Xms128m -Xmx2g -XX:SoftRefLRUPolicyMSPerMB=36000"
+export JAVA_OPTS=-Xms128m -Xmx2g -XX:SoftRefLRUPolicyMSPerMB=36000
 cd $GEOSERVER_HOME
-cmd=java "$JAVA_OPTS" -DGEOSERVER_DATA_DIR="$GEOSERVER_DATA_DIR" -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar
-nohup $cmd > log.txt &
+CMD=java $JAVA_OPTS -DGEOSERVER_DATA_DIR="$GEOSERVER_DATA_DIR" -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar
+nohup "$CMD" > log.txt &
 bash
