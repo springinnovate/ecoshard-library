cbd274358a375e1a421f544a49a66bd05218b839
Rich Sharp <richpsharp@gmail.com>
2020-04-06 12:22:42 -0700

    echo whta's going on

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -4,5 +4,6 @@ export GEOSERVER_DATA_DIR=/usr/local/geoserver/data_dir
 export JAVA_OPTS="-Xms128m -Xmx2g -XX:SoftRefLRUPolicyMSPerMB=36000"
 cd $GEOSERVER_HOME
 cmd=java "$JAVA_OPTS" -DGEOSERVER_DATA_DIR="$GEOSERVER_DATA_DIR" -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar
+echo $cmd
 nohup $cmd > log.txt &
 bash
