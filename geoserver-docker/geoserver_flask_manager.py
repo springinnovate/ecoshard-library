95e132fe7f3c2ec05b4735a6f8cefbd9fe7f565b
Rich Sharp <richpsharp@gmail.com>
2020-04-06 13:44:55 -0700

    track the flask manager

---
+++ geoserver-docker/geoserver_flask_manager.py
@@ -0,0 +1,26 @@
+"""Flask APP to manage the GeoServer."""
+import argparse
+import flask
+import urllib.parse
+
+import requests
+
+APP = flask.Flask(__name__)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='GeoServer manager')
+    parser.add_argument(
+        'geoserver_host', type=int, help='geoserver host/port to connect to')
+    parser.add_arugment(
+        'geoserver_admin_password', type=str, help='geoserver admin password')
+
+    args = parser.parse_args()
+
+    r = requests.get(
+        urllib.parse.urljoin(
+            args.geoserver_host, 'geoserver/rest/layers.json'),
+        auth=('admin', args.geoserver_admin_password))
+    result = r.json()
+    print(result)
+    for layer in result['layers']:
+        print(layer['name'])
