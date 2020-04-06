126bd2db634a87e16ab1c6a605cb77804a861c97
Rich Sharp <richpsharp@gmail.com>
2020-04-06 12:44:15 -0700

    adding explicit java path

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -2,7 +2,8 @@
 export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=/usr/local/geoserver/data_dir
 export JAVA_OPTS="-Xms512m -Xmx2g -XX:SoftRefLRUPolicyMSPerMB=36000"
+export JAVA_BIN=/usr/bin/java
 cd $GEOSERVER_HOME
-CMD="java $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar"
+CMD="$JAVA_BIN $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar"
 nohup "$CMD" > log.txt &
 bash
