89b99df179f9ceb9e4a7455247bca650fb2d642d
Rich Sharp <richpsharp@gmail.com>
2020-04-11 12:24:56 -0700

    get external ip

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -3,7 +3,7 @@ export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=/usr/local/geoserver/data_dir
 export JAVA_OPTS="-Xms512m -Xmx2g -XX:SoftRefLRUPolicyMSPerMB=36000"
 export JAVA_BIN=/usr/bin/java
-EXTERNAL_IP=$1
+export EXTERNAL_IP=$1
 cd $GEOSERVER_HOME
 nohup $JAVA_BIN $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar > geo_log.txt &
 touch geo_log.txt
