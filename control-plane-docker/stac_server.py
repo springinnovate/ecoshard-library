9e127fc84d65c8686d47563794a9c3daace8140b
Rich Sharp <richpsharp@gmail.com>
2020-04-18 16:44:10 -0700

    adding ocllections and search

--- control-plane-docker/stac_server.py
+++ control-plane-docker/stac_server.py
@@ -1,11 +1,65 @@
 """Implementation of STAC API for Salo."""
+import argparse
+import urllib.parse
+
 import flask
 
 APP = flask.Flask(__name__)
+TITLE = 'SALO API'
+
+
+@APP.route('/api/v1')
+def capabilities():
+    """Essential characteristics of this API."""
+    response_json = {
+        'title': TITLE,
+        'description': TITLE,
+        'links': [
+            {
+                'href': flask.url_for('capabilities', _external=True),
+                'rel': 'self',
+                'type': 'application/json',
+                'title': 'this document',
+                },
+            {
+                'href': flask.url_for('collections', _external=True),
+                'rel': 'data',
+                'type': 'application/json',
+                'title': 'Information about the feature collections',
+                },
+            {
+                'href': flask.url_for('search', _external=True),
+                'rel': 'search',
+                'type': 'application/json',
+                'title': 'Search across feature collections',
+                },
+            ],
+        'stac_version': '0.9.0',
+        'id': 'naip',
+        }
+    return response_json
+
+
+@APP.route('/api/v1/collections')
+def collections():
+    pass
+
+
+@APP.route('/api/v1/search')
+def search():
+    pass
+
 
 if __name__ == '__main__':
-    # wait for API calls
-    APP.config.update(SERVER_NAME=f'{args.external_ip}:{MANAGER_PORT}')
-    APP.run(host='0.0.0.0', port=MANAGER_PORT)
+    parser = argparse.ArgumentParser(description='launch STAC server')
+    parser.add_argument(
+        '--external_ip', type=str, default='localhost',
+        help='external ip of this host')
+    parser.add_argument(
+        '--external_port', type=str, default='8080',
+        help='external port of this host')
+    args = parser.parse_args()
 
-    pass
+    # wait for API calls
+    APP.config.update(SERVER_NAME=f'{args.external_ip}:{args.external_port}')
+    APP.run(host='0.0.0.0', port=args.external_port)
