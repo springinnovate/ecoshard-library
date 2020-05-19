9cd78e3580563101ee97f69e00071f29d3e27e4d
Rich Sharp <richpsharp@gmail.com>
2020-05-18 18:06:53 -0700

    get the geoserver started again

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -6,9 +6,9 @@ export JAVA_BIN=/usr/bin/java
 
 cd $GEOSERVER_HOME
 touch bin/nohup.out
-# nohup $JAVA_BIN $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar > geo_log.txt &
-# touch geo_log.txt
-# tail -n +0 --pid=$$ -f ./geo_log.txt | { sed "/Server:main: Started/ q" && kill $$ ;}
+nohup $JAVA_BIN $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar > geo_log.txt &
+touch geo_log.txt
+tail -n +0 --pid=$$ -f ./geo_log.txt | { sed "/Server:main: Started/ q" && kill $$ ;}
 cd bin
 
 touch stac_api/config.py
@@ -16,8 +16,9 @@ echo "SERVER_NAME = '$1:8888'" >> stac_api/config.py
 echo "SECRET_KEY = '$2'" >> stac_api/config.py
 
 nohup waitress-serve --listen=0.0.0.0:8888 --call stac_api:create_app &
-bash
 
-# nohup python3 geoserver_flask_manager.py --external_ip $EXTERNAL_IP --debug_api_key debug_api &
-# sleep 2
-# python3 api_key_manager.py --create --add_permission WRITE:* READ:* >> api_key
+sleep 2
+
+python3 api_key_manager.py --create --add_permission WRITE:* READ:* >> api_key
+cat api_key
+bash
