080450834f204fc6c3b7d9c760d3fc0017c3b205
Rich Sharp <richpsharp@gmail.com>
2020-05-18 17:39:59 -0700

    listen on 0.0.0.0

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -18,7 +18,7 @@ echo "SECRET_KEY = '$2'" >> app.py
 #nohup waitress-serve --listen=$1:8888 --call stac_api:create_app > stac_api_log.txt &
 bash
 
-waitress-serve --listen=maps.salo.ai:8888 --call stac_api:create_app
+waitress-serve --listen=0.0.0.0:8888 --call stac_api:create_app
 
 # nohup python3 geoserver_flask_manager.py --external_ip $EXTERNAL_IP --debug_api_key debug_api &
 # sleep 2
