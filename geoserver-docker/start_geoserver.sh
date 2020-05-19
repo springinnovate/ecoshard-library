2c02ff25a52e69d01dc557941446b0474ba8bd64
Rich Sharp <richpsharp@gmail.com>
2020-05-19 12:45:00 -0700

    add command line args for memory size and also use pgp 2.0.0

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
+# $1 -- external hostname/ip, $2 -- secret key, $3 max ram in g (i.e. 55g)
 export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=${GEOSERVER_HOME}/data_dir
-export JAVA_OPTS="-Xms2g -Xmx55g -XX:SoftRefLRUPolicyMSPerMB=36000"
+export JAVA_OPTS="-Xms2g -Xmx$3 -XX:SoftRefLRUPolicyMSPerMB=36000"
 export JAVA_BIN=/usr/bin/java
 
 cd $GEOSERVER_HOME
+echo $JAVA_OPTS >> java_opts.txt
+
 touch bin/nohup.out
 nohup $JAVA_BIN $JAVA_OPTS -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar > geo_log.txt &
 touch geo_log.txt
