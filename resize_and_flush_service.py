75312b581148fb960af78ccb92d5f29ff99f0964
Rich Sharp <richpsharp@gmail.com>
2020-05-26 09:40:57 -0700

    complete disk update method

--- resize_and_flush_service.py
+++ resize_and_flush_service.py
@@ -1,9 +1,13 @@
 """Service to monitor for new snapshots and create disk & flush if so."""
 import argparse
+import datetime
 import socket
 import subprocess
+import uuid
 
 import flask
+import requests
+import stac_spec
 
 APP = flask.Flask(__name__)
 
@@ -20,6 +24,7 @@ LAST_MOUNT_DEV_INDEX = 0
 def swap_new_disk():
     """Try to swap in a new disk if one is available."""
     try:
+        global STATUS_STRING
         STATUS_STRING = 'search for new snapshot'
         snapshot_query = subprocess.run([
             "gcloud", "compute", "snapshots", "list", "--limit=1",
@@ -28,73 +33,76 @@ def swap_new_disk():
         snapshot_name = snapshot_query.stdout.rstrip().decode('utf-8')
 
         global DISK_ITERATION
+        global LAST_SNAPSHOT_NAME
         if (snapshot_name == LAST_SNAPSHOT_NAME and
                 LAST_SNAPSHOT_NAME is not None):
             # not a new snapshot
-            STATUS_STRING = f'on iteration {DISK_ITERATION}'
+            STATUS_STRING = f'last checked: {str(datetime.datetime.now())}'
             return
 
+        # create new disk
         hostname = socket.gethostname()
-        disk_name = f'{hostname}-data-{DISK_ITERATION}'
-        DISK_ITERATION += 1
-
+        disk_name = (f'{hostname}-data-{uuid.uuid4().hex}')[:59]
         STATUS_STRING = f'creating disk {disk_name}'
+        DISK_ITERATION += 1
         subprocess.run([
             "gcloud", "compute", "disks", "create", disk_name,
             f"--source-snapshot={snapshot_name}", "--zone=us-west1-b"])
 
-        global STATUS_STRING
+        # attach the new disk to the current host
         STATUS_STRING = f'attaching disk {disk_name}'
-
         subprocess.run([
             "gcloud", "compute", "instances", "attach-disk", hostname,
             f"--disk={disk_name}", "--zone=us-west1-b"])
 
+        # unmount the current disk if any is mounted
+        global MOUNT_POINT
         if LAST_SNAPSHOT_NAME:
             STATUS_STRING = f'unmounting {MOUNT_POINT}'
             subprocess.run(["umount", MOUNT_POINT])
 
+        LAST_SNAPSHOT_NAME = snapshot_name
+
+        # mount the new disk at the mount point
+        global LAST_MOUNT_DEV_INDEX
         mount_device = POSSIBLE_MOUNT_DEVS[LAST_MOUNT_DEV_INDEX]
         LAST_MOUNT_DEV_INDEX = (
             LAST_MOUNT_DEV_INDEX+1) % len(POSSIBLE_MOUNT_DEVS)
         STATUS_STRING = f'mounting {mount_device} at {MOUNT_POINT}'
         subprocess.run(["mount", mount_device, MOUNT_POINT])
 
-        LAST_SNAPSHOT_NAME = snapshot_name
-
-
-
-        # TODO: detach the old disk
-        # gcloud compute instances detach-disk `hostname` --disk=stac-geoserver-manager-data-3 --zone=us-west1-b
-
+        # Detach and delete the old disk
         global LAST_DISK_NAME
         if LAST_DISK_NAME:
-            STATUS_STRING = f'detaching {LAST_DISK_NAME}'
+            STATUS_STRING = f'detatch old {LAST_DISK_NAME}'
+            subprocess.run([
+                "gcloud", "compute", "instances", "detach-disk", "`hostname`",
+                f"--disk={LAST_DISK_NAME}", "--zone=us-west1-b"])
 
-
-            STATUS_STRING = f'deleting {LAST_DISK_NAME}'
+            STATUS_STRING = f'deleting old {LAST_DISK_NAME}'
             subprocess.run([
                 "gcloud", "compute", "disks", "delete", LAST_DISK_NAME,
                 "--zone=us-west1-b"])
         LAST_DISK_NAME = disk_name
 
-        # TOOD: refresh the GeoServer
-        # 'geoserver' is the default geoserver password, we'll need to be
-        # authenticated to do the push
+        # refresh the GeoServer
         STATUS_STRING = f'refreshing geoserver'
         with open(PASSWORD_FILE_PATH, 'r') as password_file:
             master_geoserver_password = password_file.read()
         session = requests.Session()
         session.auth = ('admin', master_geoserver_password)
 
-        password_update_request = do_rest_action(
+        refresh_geoserver = stac_spec.do_rest_action(
             session.post,
-            f'http://{LOCAL_GEOSERVER}',
+            f'http://localhost:8080',
             'geoserver/rest/reload')
-        STATUS_STRING = f'on iteration {DISK_ITERATION}'
-
+        if refresh_geoserver:
+            STATUS_STRING = f'on iteration {DISK_ITERATION}'
+        else:
+            raise RuntimeError(
+                f'update failed: {str(refresh_geoserver)}')
     except Exception as e:
-        STATUS_STRING = f'error: {STATUS_STRING}'
+        STATUS_STRING = f'error: {str(e)}'
 
 
 @APP.route('/', methods=['GET'])
