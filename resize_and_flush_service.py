087274ab029587d71e8bbb566952c4f18d726031
Rich Sharp <richpsharp@gmail.com>
2020-05-26 11:31:48 -0700

    return right away so we don't waste disks

--- resize_and_flush_service.py
+++ resize_and_flush_service.py
@@ -71,6 +71,7 @@ def swap_new_disk():
                 "--format=value(name)"], stdout=subprocess.PIPE)
             snapshot_name = snapshot_query.stdout.rstrip().decode('utf-8')
             STATUS_STRING = f'creating new disk from snapshot {snapshot_name}'
+            return
             LOGGER.info(STATUS_STRING)
             global DISK_ITERATION
             global LAST_SNAPSHOT_NAME
