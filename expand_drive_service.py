a7736a44b63b044280a82da5441b22ea28f60eae
Rich Sharp <richpsharp@gmail.com>
2020-05-28 18:14:19 -0700

    get that zone in for reseize

--- expand_drive_service.py
+++ expand_drive_service.py
@@ -38,7 +38,7 @@ def resize():
     # resize the google disk
     subprocess.run([
         'gcloud', 'compute', 'disks', 'resize', DISK_NAME, '--size',
-        f'{disk_size_gb+gb_to_add}'], check=True)
+        f'{disk_size_gb+gb_to_add}', f'--zone={ZONE}'], check=True)
 
     # resize the file system
     subprocess.run(['resize2fs', DEVICE_NAME], check=True)
