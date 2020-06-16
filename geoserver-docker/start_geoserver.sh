d8c62c908b2d3dae297ff8d73d4981611f9ec7c4
Rich Sharp <richpsharp@gmail.com>
2020-06-16 11:13:41 -0700

    differentiate between internal and external host

--- geoserver-docker/start_geoserver.sh
+++ geoserver-docker/start_geoserver.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -x
-# $1 -- external API host
-# $2 -- external API port
-# $3 -- GEOSERVER host
-# $4 -- GEOSERVER port
-# $5 -- max ram in g (i.e. 55g)
-# $6 -- authorization database password
-# $7 -- authorization database host
+# $1 -- internal API host
+# $2 -- internal API port
+# $3 -- external API host:port
+# $4 -- GEOSERVER host
+# $5 -- GEOSERVER port
+# $6 -- max ram in g (i.e. 55g)
+# $7 -- authorization database password
+# $8 -- authorization database host
 export GEOSERVER_HOME=/usr/local/geoserver
 export GEOSERVER_DATA_DIR=${GEOSERVER_HOME}/data_dir
-export JAVA_OPTS="-Xms2g -Xmx$5 -XX:SoftRefLRUPolicyMSPerMB=36000 -server -XX:+UseParallelGC -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR"
+export JAVA_OPTS="-Xms2g -Xmx$6 -XX:SoftRefLRUPolicyMSPerMB=36000 -server -XX:+UseParallelGC -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR"
 export JAVA_BIN=/usr/bin/java
 
 # cd $GEOSERVER_HOME
@@ -20,7 +21,8 @@ cd bin
 
 touch stac_api/config.py
 echo "SERVER_NAME = '$1:$2'" > stac_api/config.py
-echo "GEOSERVER_HOST = '$3:$4'" >> stac_api/config.py
+echo "EXTERNAL_API_HOST = '$3'"
+echo "GEOSERVER_HOST = '$4:$5'" >> stac_api/config.py
 echo "SECRET_KEY = 'none'" >> stac_api/config.py
-echo "SQLALCHEMY_DATABASE_URI = 'postgresql://salo-api-user:$6@$7/salo-api-auth'" >> stac_api/config.py
-waitress-serve --listen=0.0.0.0:8888 --call stac_api:create_app
+echo "SQLALCHEMY_DATABASE_URI = 'postgresql://salo-api-user:$7@$8/salo-api-auth'" >> stac_api/config.py
+waitress-serve --listen=0.0.0.0:$2 --call stac_api:create_app
