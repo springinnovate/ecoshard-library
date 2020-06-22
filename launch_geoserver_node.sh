6a6b30ac59142da56834b8fe02cee6ea56016026
Rich Sharp <richpsharp@gmail.com>
2020-06-22 11:28:04 -0700

    ;only do it if file exists

--- launch_geoserver_node.sh
+++ launch_geoserver_node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
-source stac_envs
-docker-compose -f compose-geoserver-node.yml up --remove-orphans --build
+source stac_envs &&
+    docker-compose -f compose-geoserver-node.yml up --remove-orphans --build
