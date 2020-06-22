b1a00bccfafff592dfd4c5c639ddc879536bf934
Rich Sharp <richpsharp@gmail.com>
2020-06-22 14:39:41 -0700

    trying to get geoserver dirs right

--- docker_contexts/geoserver_node_docker/start_geoserver.sh
+++ docker_contexts/geoserver_node_docker/start_geoserver.sh
@@ -1,7 +1,6 @@
-geoserver-docker/start_geoserver.sh
 #!/bin/bash -x
 # $1 -- max ram in g (i.e. 5g)
-export GEOSERVER_HOME=/usr/local/geoserver
+export GEOSERVER_HOME=/opt/tomcat/webapps/geoserver
 export GEOSERVER_DATA_DIR=${GEOSERVER_HOME}/data_dir
 export JAVA_OPTS="-Xms2g -Xmx$1 -XX:SoftRefLRUPolicyMSPerMB=36000 -server -XX:+UseParallelGC -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR"
 export JAVA_BIN=/usr/bin/java
