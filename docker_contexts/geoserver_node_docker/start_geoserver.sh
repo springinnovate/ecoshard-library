fc08086e395b9da8839c5c214d85d07468bddfcc
Rich Sharp <richpsharp@gmail.com>
2020-06-22 14:25:56 -0700

    starting geoserver with simple tomcat

---
+++ docker_contexts/geoserver_node_docker/start_geoserver.sh
@@ -0,0 +1,12 @@
+geoserver-docker/start_geoserver.sh
+#!/bin/bash -x
+# $1 -- max ram in g (i.e. 5g)
+export GEOSERVER_HOME=/usr/local/geoserver
+export GEOSERVER_DATA_DIR=${GEOSERVER_HOME}/data_dir
+export JAVA_OPTS="-Xms2g -Xmx$1 -XX:SoftRefLRUPolicyMSPerMB=36000 -server -XX:+UseParallelGC -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR"
+export JAVA_BIN=/usr/bin/java
+
+cd $GEOSERVER_HOME
+
+touch bin/nohup.out
+/opt/tomcat/bin/catalina.sh run > tomcatlog.txt
