7ebd45f46cc356a2bf8197080ea676258b37c0a1
Rich Sharp <richpsharp@gmail.com>
2020-06-25 10:27:40 -0700

    flag in wrong spot

--- launch_stac_test.sh
+++ launch_stac_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose -f compose-stac-manager.yml up db test --remove-orphans --build $@
+    docker-compose -f compose-stac-manager.yml up --remove-orphans --build db test $@
