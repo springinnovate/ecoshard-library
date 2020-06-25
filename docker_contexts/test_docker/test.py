d650a577e7ce12b4d6110e829053034f60cf2e78
Rich Sharp <richpsharp@gmail.com>
2020-06-25 11:07:51 -0700

    getting a db together for a flask app

--- docker_contexts/test_docker/test.py
+++ docker_contexts/test_docker/test.py
@@ -1,15 +1,19 @@
+"""Tracer code for setting up sqlalchemy."""
+import argparse
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    #'${DB_USER}', '${DB_PASSWORD}', 'db:5432'
+    parser.add_argument('dbuser', type=str, help='database username')
+    parser.add_argument('dbpassword', type=str, help='database password')
+    parser.add_argument('dbhost', type=str, help='database host')
+    args = parser.parse_args()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{args.dbuser}:{args.dbpassword}@{args.dbhost}'
+    db = SQLAlchemy(app)
