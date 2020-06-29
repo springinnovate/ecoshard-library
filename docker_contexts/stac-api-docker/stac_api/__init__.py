f6347ea9b9f132f6bc3f0ae42d8d49da35e10ab3
Rich Sharp <richpsharp@gmail.com>
2020-06-29 11:58:03 -0700

    track models for the stac view

--- docker_contexts/stac-api-docker/stac_api/__init__.py
+++ docker_contexts/stac-api-docker/stac_api/__init__.py
@@ -1,12 +1,13 @@
 # sitemaker/__init__.py
 
 from flask import Flask
-from .auth import auth_bp, db
+from .auth import auth_bp, auth_db
+from .stac import stac_bp, stac_db
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-db.init_app(app)
-migrate = Migrate(app, db)
+auth_db.init_app(app)
+migrate = Migrate(app, auth_db)
 
 app.register_blueprint(auth_bp, url_prefix="/users")
