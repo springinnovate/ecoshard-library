8cea9d5b6e373860053de7552587ff9c96db94de
Rich Sharp <richpsharp@gmail.com>
2020-07-01 20:13:21 -0700

    typo

--- docker_contexts/expand_drive_service/launch_expand_drive_service.sh
+++ docker_contexts/expand_drive_service/launch_expand_drive_service.sh
@@ -2,4 +2,4 @@
 # first authenticate gsutil
 /usr/local/gcloud-sdk/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/run/secrets/service_account_key
 # launch the service
-python ./expand_drive_serivce.py $@
+python ./expand_drive_service.py $@
