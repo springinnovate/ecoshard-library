4153072f8fe4b3c34c9004acaae5994afb2de955
Rich Sharp <richpsharp@gmail.com>
2020-07-01 17:30:58 -0700

    bring up drive service not test

--- launch_stac_test.sh
+++ launch_stac_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose -f compose-stac-manager.yml up --remove-orphans --build db test $@
+    docker-compose -f compose-stac-manager.yml up --remove-orphans --build expand-drive-service $@
