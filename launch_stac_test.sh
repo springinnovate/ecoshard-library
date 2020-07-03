865815aea6b7f7c29803513d93ca185bc2ca3678
Rich Sharp <richpsharp@gmail.com>
2020-07-02 20:14:32 -0700

    compose with detach

--- launch_stac_test.sh
+++ launch_stac_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose -f compose-stac-manager.yml up --remove-orphans --build expand_drive_service geoserver $@
+    docker-compose -d -f compose-stac-manager.yml up --remove-orphans --build expand_drive_service geoserver $@
