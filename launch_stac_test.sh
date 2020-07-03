3f2515d1b510a8bc12761deb98c39dbdf029177c
Rich Sharp <richpsharp@gmail.com>
2020-07-02 20:14:58 -0700

    -d doesn't work

--- launch_stac_test.sh
+++ launch_stac_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose -d -f compose-stac-manager.yml up --remove-orphans --build expand_drive_service geoserver $@
+    docker-compose -f compose-stac-manager.yml up --remove-orphans --build expand_drive_service geoserver $@
