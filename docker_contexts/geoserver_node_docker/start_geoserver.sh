6beb2db4eda397de5f848f2d41d86dc170b4402c
Rich Sharp <richpsharp@gmail.com>
2020-07-05 15:07:00 -0700

    make geoserver data dir an environment variable for the container

--- docker_contexts/geoserver_node_docker/start_geoserver.sh
+++ docker_contexts/geoserver_node_docker/start_geoserver.sh
@@ -5,4 +5,4 @@ export JAVA_OPTS="-Xms2g -Xmx$1 -XX:SoftRefLRUPolicyMSPerMB=36000 -server -XX:+U
 export JAVA_BIN=/usr/bin/java
 
 cd $GEOSERVER_HOME
-/opt/tomcat/bin/catalina.sh run > tomcatlog.txt
+/opt/tomcat/bin/catalina.sh run
