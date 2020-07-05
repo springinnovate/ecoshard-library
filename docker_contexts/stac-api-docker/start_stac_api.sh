c8c26bdf423f4ef66eda2937cd59f46fad601463
Rich Sharp <richpsharp@gmail.com>
2020-07-05 15:54:12 -0700

    why is config in stac_api and not root?

--- docker_contexts/stac-api-docker/start_stac_api.sh
+++ docker_contexts/stac-api-docker/start_stac_api.sh
@@ -2,8 +2,8 @@
 # Authenticate the gcloud SDK so it can read from buckets
 /usr/local/gcloud-sdk/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${BUCKET_READ_SERVICE_ACCOUNT_KEYFILE}
 
-touch stac_api/config.py
-echo "GEOSERVER_MANAGER_HOST = '${GEOSERVER_MANAGER_HOST}'" >> stac_api/config.py
-echo "SECRET_KEY = '${API_FLASK_SECRET_KEY}'" >> stac_api/config.py
-echo "SQLALCHEMY_DATABASE_URI = '${SQLALCHEMY_DATABASE_URI}" >> stac_api/config.py
+touch config.py
+echo "GEOSERVER_MANAGER_HOST = '${GEOSERVER_MANAGER_HOST}'" >> config.py
+echo "SECRET_KEY = '${API_FLASK_SECRET_KEY}'" >> config.py
+echo "SQLALCHEMY_DATABASE_URI = '${SQLALCHEMY_DATABASE_URI}" >> config.py
 waitress-serve --listen=0.0.0.0:${API_HOST_PORT} --call stac_api:create_app 2>&1
