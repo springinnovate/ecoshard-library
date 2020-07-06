0597f5fac052dae59222e4f8a40f2cd77713728e
Rich Sharp <richpsharp@gmail.com>
2020-07-06 11:59:09 -0700

    expose tracebacks

--- docker_contexts/stac-api-docker/start_stac_api.sh
+++ docker_contexts/stac-api-docker/start_stac_api.sh
@@ -7,4 +7,4 @@ echo "GEOSERVER_DATA_DIR = '${GEOSERVER_DATA_DIR}'" >> stac_api/config.py
 echo "GEOSERVER_MANAGER_HOST = '${GEOSERVER_MANAGER_HOST}'" >> stac_api/config.py
 echo "SECRET_KEY = '${API_FLASK_SECRET_KEY}'" >> stac_api/config.py
 echo "SQLALCHEMY_DATABASE_URI = '${SQLALCHEMY_DATABASE_URI}'" >> stac_api/config.py
-waitress-serve --listen=0.0.0.0:${API_HOST_PORT} --call stac_api:create_app 2>&1
+waitress-serve --expose-tracebacks --listen=0.0.0.0:${API_HOST_PORT} --call stac_api:create_app 2>&1
