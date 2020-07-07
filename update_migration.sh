1950a5adbd553409904c3b63f940ac98f6848b38
Rich Sharp <richpsharp@gmail.com>
2020-07-06 18:31:34 -0700

    override with entrypoint

--- update_migration.sh
+++ update_migration.sh
@@ -2,5 +2,4 @@
 set -a
 source stac_envs &&
     docker-compose build
-    docker run --rm -it --command update_migration.sh -v `pwd`/docker_contexts/stac-api-docker:/usr/local/stac_manager/ stac_manager
-docker run -t
+    docker run --rm -it --entrypoint ./update_migration.sh -v `pwd`/docker_contexts/stac-api-docker:/usr/local/stac_manager/ stac_manager
