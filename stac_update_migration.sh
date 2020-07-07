22c831f81a0a467034e70d3e6a6af721b76ae5bc
Rich Sharp <richpsharp@gmail.com>
2020-07-06 19:02:41 -0700

    service goes last

--- stac_update_migration.sh
+++ stac_update_migration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose -f docker-compose.yml -f migrate.yml up db stac_manager --build --remove-orphans
+    docker-compose -f docker-compose.yml -f migrate.yml up --build --remove-orphans db stac_manager
