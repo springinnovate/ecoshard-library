a28813ff113f85618ffc3228dc0eeded726eb9d1
Rich Sharp <richpsharp@gmail.com>
2020-07-06 19:01:08 -0700

    use simpler name

--- stac_update_migration.sh
+++ stac_update_migration.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    docker-compose build
-    docker run --rm -it --entrypoint ./update_migration.sh -v `pwd`/docker_contexts/stac-api-docker:/usr/local/stac_manager/ stac_manager
+    docker-compose -f docker-compose.yml -f migrate.yml up --build --remove-orphans
