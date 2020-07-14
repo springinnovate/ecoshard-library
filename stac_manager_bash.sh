491e3a6e60b9fdcc3a901589e20881c27d328758
Rich Sharp <richpsharp@gmail.com>
2020-07-14 11:45:51 -0700

    deleting unused configuration files

--- stac_manager_bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -a
-source stac_envs &&
-    docker-compose -f docker-compose.yml -f stac-bash.yml up --build --remove-orphans stac_manager
--- stac_update_migration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -a
-source stac_envs &&
-    docker-compose -f docker-compose.yml -f migrate.yml up --build --remove-orphans db stac_manager
