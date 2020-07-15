1de34ac99c6054366b8b4297034dafe177dd8bbb
Rich Sharp <richpsharp@gmail.com>
2020-07-14 17:44:13 -0700

    do a db upgrade on launch and also start the stac api

--- docker_contexts/stac-api-docker/start_stac_api.sh
+++ docker_contexts/stac-api-docker/start_stac_api.sh
@@ -2,5 +2,8 @@
 # Authenticate the gcloud SDK so it can read from buckets
 /usr/local/gcloud-sdk/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${BUCKET_READ_SERVICE_ACCOUNT_KEYFILE}
 
+echo "upgrade db"
+flask db upgrade
+
 echo "launching app"
 waitress-serve --expose-tracebacks --listen=0.0.0.0:${API_HOST_PORT} --call stac_api:create_app 2>&1
