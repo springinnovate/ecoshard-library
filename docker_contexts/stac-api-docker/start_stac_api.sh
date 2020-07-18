a1a7c255749ef22220784d6374ca3bb9f59593b3
Rich Sharp <richpsharp@gmail.com>
2020-07-17 20:56:40 -0700

    export not set

--- docker_contexts/stac-api-docker/start_stac_api.sh
+++ docker_contexts/stac-api-docker/start_stac_api.sh
@@ -3,9 +3,9 @@
 /usr/local/gcloud-sdk/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${BUCKET_READ_SERVICE_ACCOUNT_KEYFILE}
 
 echo "upgrade db"
-set FLASK_INITALIZE_ONLY=1
+export FLASK_INITALIZE_ONLY=1
 flask db upgrade
 
 echo "launching app"
-set FLASK_INITALIZE_ONLY=0
+export FLASK_INITALIZE_ONLY=0
 waitress-serve --expose-tracebacks --listen=0.0.0.0:8888 --call stac_api:create_app 2>&1
