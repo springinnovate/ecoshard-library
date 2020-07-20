b02a88e28606fbf86112aee1348db11a2ceb22e4
Rich Sharp <richpsharp@gmail.com>
2020-07-19 22:28:27 -0700

    it's python 3

--- launch_geoserver_node_service.sh
+++ launch_geoserver_node_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a
 source stac_envs &&
-    python geoserver_node_drive_mirror_service.py --app_port ${GEOSERVER_HEALTH_PORT} --snapshot_pattern ${GEOSERVER_SNAPSHOT_PATTERN} --mount_point ${GEOSERVER_DATA_DISK_VOLUME} --check_time 300 --service_name geoserver --google_cloud_zone ${GOOGLE_CLOUD_ZONE}
+    python3 geoserver_node_drive_mirror_service.py --app_port ${GEOSERVER_HEALTH_PORT} --snapshot_pattern ${GEOSERVER_SNAPSHOT_PATTERN} --mount_point ${GEOSERVER_DATA_DISK_VOLUME} --check_time 300 --service_name geoserver --google_cloud_zone ${GOOGLE_CLOUD_ZONE}
